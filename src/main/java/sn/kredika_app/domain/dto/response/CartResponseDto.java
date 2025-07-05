8ac0e6ce509a0d3d277306eca341683f15dc9a69
SNBAM <mba@modelsis.sn>
2025-07-05 02:59:15 +0000

    branch develop

--- src/main/java/sn/kredika_app/domain/dto/response/CartResponseDto.java
+++ src/main/java/sn/kredika_app/domain/dto/response/CartResponseDto.java
@@ -1,10 +1,23 @@
 package sn.kredika_app.domain.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CartResponseDto {
     private UUID id;
     private String statusCode;
